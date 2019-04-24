31ea1cad385f5962b3a266507d6acfd2ee2b9b6a
so6370 <timy117@gmail.com>
2019-04-24 17:01:19 +0800

    Update utils.py
    Add fn '__set_household_data' and private var 'crop_set'

--- rdlfs/utils.py
+++ rdlfs/utils.py
@@ -7,7 +7,7 @@ class ExcelHandler:
     titles = {
         'sample': ['農戶編號', '調查姓名', '電話', '地址', '出生年', '原層別', '連結編號'],
         'household': ['出生年', '關係'],
-        'crop_sbdy': ['[轉作補貼]', '項目', '作物名稱', '金額', '期別'],
+        'crop_sbdy': ['[轉作補貼]', '項目', '作物名稱', '期別'],
         'disaster': ['[災害]', '項目', '災害', '核定作物', '核定面積', '金額'],
         '104_mon_hire_fir_half': ['[104年農普每月僱工]', '一月', '二月', '三月', '四月', '五月', '六月'],
         '104_mon_hire_sec_half': ['七月', '八月', '九月', '十月', '十一月', '十二月'],
@@ -20,6 +20,7 @@ class ExcelHandler:
         self.__row_index = 1
         self.__wb = openpyxl.Workbook()
         self.__sheet = self.__wb.active
+        self.__crop_set = {}
 
         self.__set_column_width()
 
@@ -57,7 +58,7 @@ class ExcelHandler:
         self.row_index = 1
 
     def __set_sample_base_data(self, data):
-        self.__set_title(self.titles['sample'])
+        self.__set_title('sample')
         value_list = [data['farmer_num'], data['name'], data['tel'],
                       data['addr'], data['birthday'], data['layer'], data['link_num']]
 
@@ -69,7 +70,7 @@ class ExcelHandler:
         self.row_index = 1
 
     def __set_household_data(self, members):
-        self.__set_title(self.titles['household'])
+        self.__set_title('household')
         if members:
             for person in members:
                 self.row_index = 1
@@ -77,6 +78,23 @@ class ExcelHandler:
                     self.__sheet.cell(column=index, row=self.row_index).value = val
                     self.__sheet.cell(column=index, row=self.row_index).alignment = Alignment(horizontal='left')
 
+    def __set_crop_sbdy_data(self, crop_sbdy_list):
+        if crop_sbdy_list:
+            self.row_index = 2
+            self.__set_title('crop_sbdy')
+            self.__crop_set = crop_name = {i[0] for i in crop_sbdy_list}
+
+            for index, val in enumerate(crop_name, start=1):
+                if index >= 2:
+                    self.row_index = 1
+                self.__sheet.cell(column=2, row=self.row_index).value = index
+                self.__sheet.cell(column=3, row=self.row_index).value = val
+                self.__sheet.cell(column=4, row=self.row_index).value = '1'
+
+                if len(val) > 8:
+                    self.__sheet.cell(column=3, row=self.row_index).alignment = Alignment(wrap_text=True)
+
     def set_data(self, data):
         self.__set_sample_base_data(data)
         self.__set_household_data(data['household'])
+        self.__set_crop_sbdy_data(data['crop_sbdy'])
