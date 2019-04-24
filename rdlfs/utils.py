709661f73a64d28271ade4db597b3dbe1ff563ce
so6370 <timy117@gmail.com>
2019-04-24 17:48:36 +0800

    Update utils.py
    Add fn '__set_disaster_data'

--- rdlfs/utils.py
+++ rdlfs/utils.py
@@ -8,7 +8,7 @@ class ExcelHandler:
         'sample': ['農戶編號', '調查姓名', '電話', '地址', '出生年', '原層別', '連結編號'],
         'household': ['出生年', '關係'],
         'crop_sbdy': ['[轉作補貼]', '項目', '作物名稱', '期別'],
-        'disaster': ['[災害]', '項目', '災害', '核定作物', '核定面積', '金額'],
+        'disaster': ['[災害]', '項目', '災害', '核定作物', '核定面積'],
         '104_mon_hire_fir_half': ['[104年農普每月僱工]', '一月', '二月', '三月', '四月', '五月', '六月'],
         '104_mon_hire_sec_half': ['七月', '八月', '九月', '十月', '十一月', '十二月'],
         '106_shirt_hire_fir_half': ['一月', '二月', '三月', '四月', '五月', '六月'],
@@ -20,7 +20,7 @@ class ExcelHandler:
         self.__row_index = 1
         self.__wb = openpyxl.Workbook()
         self.__sheet = self.__wb.active
-        self.__crop_set = {}
+        self.__crop_set = set()
 
         self.__set_column_width()
 
@@ -94,7 +94,29 @@ class ExcelHandler:
                 if len(val) > 8:
                     self.__sheet.cell(column=3, row=self.row_index).alignment = Alignment(wrap_text=True)
 
+    def __set_disaster_data(self, disaster_list):
+        if disaster_list:
+            _dict = {}
+            self.row_index = 2
+            for d in disaster_list:
+                key = (d[0], d[1])
+                if key not in _dict:
+                    _dict[key] = float(d[2])
+                else:
+                    _dict[key] = _dict.get(key) + float(d[2])
+            self.__set_title('disaster')
+
+            for index, _tuple in enumerate(_dict.items(), start=1):
+                if index >= 2:
+                    self.row_index = 1
+                self.__crop_set.add(_tuple[0][1])
+                self.__sheet.cell(column=2, row=self.row_index).value = index
+                self.__sheet.cell(column=3, row=self.row_index).value = _tuple[0][0]
+                self.__sheet.cell(column=4, row=self.row_index).value = _tuple[0][1]
+                self.__sheet.cell(column=5, row=self.row_index).value = _tuple[1]
+
     def set_data(self, data):
         self.__set_sample_base_data(data)
         self.__set_household_data(data['household'])
         self.__set_crop_sbdy_data(data['crop_sbdy'])
+        self.__set_crop_sbdy_data(data['disaster'])
